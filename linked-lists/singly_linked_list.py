8834f03654c0497b0ab305782caa651d3c2212db
simonbcodes <simonbukin@gmail.com>
2019-08-04 21:33:01 -0700

    add LinkedLists tests and finish prepend and delete_by_value functions

--- linked-lists/singly_linked_list.py
+++ linked-lists/singly_linked_list.py
@@ -1,9 +1,11 @@
 class Node:
+    """ Node containing a single piece of data. """
     def __init__(self, data):
         self.next = None
         self.data = data
 
 class LinkedList:
+    """ Singly LinkedList, made up of connected Nodes. """
     def __init__(self, data=None):
         if data:
             self.head = Node(data)
@@ -13,13 +15,13 @@ class LinkedList:
     def __repr__(self):
         ll_to_arr = []
         cur = self.head
-        while(cur != None):
+        while cur != None:
             ll_to_arr.append(str(cur.data))
             cur = cur.next
         return ','.join(ll_to_arr)
 
-
     def append(self, data):
+        """ Append data to the end of a LinkedList. """
         if self.head == None:
             self.head = Node(data)
         else:
@@ -29,6 +31,7 @@ class LinkedList:
             cur.next = Node(data)
 
     def prepend(self, data):
+        """ Prepend data to the beginning of a LinkedList. """
         if self.head == None:
             self.head = Node(data)
         else:
@@ -36,10 +39,20 @@ class LinkedList:
             cur.next = self.head
             self.head = cur
 
-if __name__ == '__main__':
-    ll = LinkedList(3)
-    print(ll)
-    ll.append(4)
-    print(ll)
-    ll.prepend(5)
-    print(ll)
+    def delete_by_value(self, data):
+        """ Delete first Node with a given value. """
+        if self.head == None:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        if self.head.next == None:
+            return
+        cur = self.head
+        while cur.next != None and cur.next.data != data:
+            cur = cur.next
+        if cur.next:
+            if cur.next.data == data:
+                cur.next = cur.next.next
+        else:
+            return
---
+++ linked-lists/test_linked_lists.py
@@ -0,0 +1,57 @@
+import pytest
+from singly_linked_list import LinkedList, Node
+
+@pytest.fixture
+def empty_linked_list():
+    """ Return an empty LinkedList. """
+    return LinkedList()
+
+@pytest.fixture
+def initialized_linked_list():
+    """ Return a LinkedList with an initialized head. """
+    return LinkedList(10)
+
+def construct_linked_list(l):
+    ll = LinkedList()
+    for i in l:
+        ll.append(i)
+    return ll
+
+def ll_to_arr(ll):
+    arr = []
+    cur = ll.head
+    while cur != None:
+        arr.append(cur.data)
+        cur = cur.next
+    return arr
+
+@pytest.fixture
+def one_to_ten_linked_list():
+    """ Return a LinkedList with Nodes from 1 to 10. """
+    ll = LinkedList
+    for val in [i for i in range(0,11)]:
+        ll.append(val)
+    return ll
+
+def test_empty_linked_list(empty_linked_list):
+    assert empty_linked_list.head == None
+
+def test_initialized_linked_list(initialized_linked_list):
+    assert initialized_linked_list.head.data == 10
+
+@pytest.mark.parametrize('init, delete, output', [
+    ([], 1, []),
+    ([1], 1, []),
+    ([1], 2, [1]),
+    ([1,2], 1, [2]),
+    ([1,2], 2, [1]),
+    ([1,2], 3, [1,2]),
+    ([1,2,3], 1, [2,3]),
+    ([1,2,3], 2, [1,3]),
+    ([1,2,3], 3, [1,2]),
+    ([1,2,3], 4, [1,2,3])
+])
+def test_delete_by_value(init, delete, output):
+    ll = construct_linked_list(init)
+    ll.delete_by_value(delete)
+    assert ll_to_arr(ll) == output
